c7d2b62240c98885bb9a6fa6928f2b319670acd5
LeeDogyeom <lywoo@ivis.ai>
2026-07-09 13:39:27 +0900

    리뷰중

--- playground/hello_world/src/main.rs
+++ playground/hello_world/src/main.rs
@@ -1,3 +1,58 @@
 fn main() {
     println!("Hello, world!");
+		let mut grade ;
+		let mut score;
+		score = 100.0;
+		grade = calculcate_grade(score);
+		println!("score: {}, grade: {}", score, grade);
+
+
+		score = 90.0;
+		grade = calculcate_grade(score);
+		println!("score: {}, grade: {}", score, grade);
+
+
+		score = 80.0;
+		grade = calculcate_grade(score);
+		println!("score: {}, grade: {}", score, grade);
+
+
+		score = 70.0;
+		grade = calculcate_grade(score);
+		println!("score: {}, grade: {}", score, grade);
+
+
+		score = 60.0;
+		grade = calculcate_grade(score);
+		println!("score: {}, grade: {}", score, grade);
+
+}
+
+// /home/ldg/Study/rust_study/playground/hello_world/Cargo.toml 에 추가함
+#[cfg(not(features = "owned_grade"))]
+// lifetime 으로 &str 로 반환 하면 안됨 &'static str 로 반환 필요
+fn calculcate_grade(score:f64) -> &'static str {
+	println!("features: not owned_grade");
+	if score >= 90.0 {
+		"A"
+	} else if score >= 80.0 {
+		"B"
+	} else if score >= 70.0 {
+		"C"
+	} else {
+		"F"
+	}
+}
+#[cfg(features = "owned_grade")]
+fn calculcate_grade(score:f64) -> String {
+	println!("features: owned_grade");
+	if score >= 90.0 {
+		"A".to_string()
+	} else if score >= 80.0 {
+		"B".to_string()
+	} else if score >= 70.0 {
+		"C".to_string()
+	} else {
+		"F".to_string()
+	}
 }
